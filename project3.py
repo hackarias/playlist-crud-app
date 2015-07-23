7e57ba549b7977e494152ee971b0f18bd0ea9106
gloison <zack.gustavsson@gmail.com>
2015-07-23 19:17:34 +0200

    Creating views for create/view/edit/delete. Creating corresponding template files

--- project3.py
+++ project3.py
@@ -8,10 +8,24 @@ def home():
     return "This will be the home page"
 
 
-@app.route('/profile/')
-def profile():
+@app.route('/profile/<int:user_id>/')
+def profile(user_id):
     return "This will be the users profile page"
 
+
+@app.route('/profile/<int:user_id>/edit/')
+def edit_profile(user_id):
+    return "This will be the Edit User page"
+
+
+@app.route('/profile/<int:user_id>/delete/')
+def delete_user(user_id):
+    return "This will be the Delete User page"
+
+
+@app.route('/profile/create/')
+def create_user():
+    return "This will be the Create User page"
 
 if __name__ == '__main__':
     app.run()
