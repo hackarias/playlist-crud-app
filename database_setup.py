f8ae8d9c2770019ea720923e361b615ff902f488
gloison <zack.gustavsson@gmail.com>
2015-09-26 21:39:26 +0200

    Add data seralization and comments

--- database_setup.py
+++ database_setup.py
@@ -6,6 +6,8 @@ base = declarative_base()
 
 
 class User(base):
+    """ Model storing information about the user. Currently it stores the users
+     id, username, name, email and picture """
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
@@ -16,6 +18,8 @@ class User(base):
 
 
 class Playlist(base):
+    """ Model storing information about the users playlist. Currently stores
+    id, name, description, user_id. """
     __tablename__ = 'playlist'
 
     id = Column(Integer, primary_key=True)
@@ -24,21 +28,43 @@ class Playlist(base):
     user_id = Column(Integer, ForeignKey('user.id'))
     user_relationship = relationship(User)
 
+    @property
+    def serialize(self):
+        """ Returns data for each object in serializable format """
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'user_id': self.user_id,
+            'user_relationship': self.user_relationship,
+        }
+
 
 class Song(base):
+    """ Model storing the information about the users songs. Currently stores
+    id, song name, artist, playlist id and user id. """
     __tablename__ = 'songs'
 
     id = Column(Integer, primary_key=True)
     song_name = Column(String(80), nullable=False)
-    # song_uri = Column(String)
     artist = Column(String, nullable=False)
-    # album_name = Column(String)
-    # album_cover = Column(String)
     playlist_id = Column(Integer, ForeignKey('playlist.id'))
     playlist_relationship = relationship(Playlist)
     user_id = Column(Integer, ForeignKey('user.id'))
     user_relationship = relationship(User)
 
+    @property
+    def serialize(self):
+        """ Returns data for each object in serializable format """
+        return {
+            'id': self.id,
+            'song_name': self.song_name,
+            'artist': self.artist,
+            'playlist_id': self.playlist_id,
+            'playlist_relationship': self.playlist_relationship,
+            'user_id': self.user_id,
+            'user_relationship': self.user_relationship,
+        }
 
 engine = create_engine('sqlite:///test.db')
 
